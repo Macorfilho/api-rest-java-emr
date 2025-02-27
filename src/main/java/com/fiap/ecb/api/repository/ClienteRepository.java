9b251677de72f5621df4117e86ab8e23fb898e62
Marcelo <macorfilho@gmail.com>
2025-02-27 09:05:04 -0300

    finalizando aula

--- src/main/java/com/fiap/ecb/api/repository/ClienteRepository.java
+++ src/main/java/com/fiap/ecb/api/repository/ClienteRepository.java
@@ -1,4 +1,4 @@
 package com.fiap.ecb.api.repository;
-
-public interface ClienteRepository {
-}
+import com.fiap.ecb.api.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {}
